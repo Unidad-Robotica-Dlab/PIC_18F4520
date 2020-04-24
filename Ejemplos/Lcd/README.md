c391fbcb29c9f5f2e6fb0f3e8d7132d4baeba892
Unidad-Robotica-Dlab <53839515+Unidad-Robotica-Dlab@users.noreply.github.com>
2020-04-24 16:09:42 -0400

    lcd

---
+++ Ejemplos/Lcd/README.md
@@ -0,0 +1,9 @@
+# Blink Led RB0
+
+Led RB0 
+
+*parpadeo led RB0 para compilar y cargar pic.
+
+*ccs 5.095
+
+*PIC18F4520
---
+++ Ejemplos/Lcd/lcd.c
@@ -0,0 +1,33 @@
+#include <config.h>
+#include <lcd.c>
+//
+//lcd ejemplo
+//
+//       #define LCD_ENABLE_PIN  PIN_E0                                    ////
+////     #define LCD_RS_PIN      PIN_E1                                    ////
+////     #define LCD_RW_PIN      PIN_E2                                    ////
+////     #define LCD_DATA4       PIN_D4                                    ////
+////     #define LCD_DATA5       PIN_D5                                    ////
+////     #define LCD_DATA6       PIN_D6                                    ////
+////     #define LCD_DATA7       PIN_D7 
+//
+
+
+#define HW_LCD_BLACKLIGHT PIN_E2
+
+
+void main()
+{
+
+
+lcd_init();
+lcd_putc("\fReady...\n");
+output_low(HW_LCD_BLACKLIGHT);
+
+
+while(1){
+
+
+
+} 
+}
